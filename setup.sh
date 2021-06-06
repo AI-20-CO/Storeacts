e5985529d3a5514d3d104eac50eb68e7db8e0928
Ayaan-20 <73228148+Ayaan-20@users.noreply.github.com>
2021-06-06 17:31:30 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,4 @@
 mkdir -p ~/.streamlit/
-[theme]
-primaryColor="#1ba7b5"
-backgroundColor="#36363D"
-secondaryBackgroundColor="#212123"
-textColor="#1BA7B5"
 echo "\
 [general]\n\
 email = \"your-email@domain.com\"\n\
@@ -13,4 +8,9 @@ echo "\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+[theme]
+primaryColor='#1ba7b5'
+backgroundColor='#36363D'
+secondaryBackgroundColor='#212123'
+textColor='#1BA7B5'
 " > ~/.streamlit/config.toml
