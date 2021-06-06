8fb4363669001763f7e3ee2d1390101f695a823f
Ayaan-20 <73228148+Ayaan-20@users.noreply.github.com>
2021-06-06 17:25:05 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 mkdir -p ~/.streamlit/
+[theme]
+primaryColor="#1ba7b5"
+backgroundColor="#36363D"
+secondaryBackgroundColor="#212123"
+textColor="#1BA7B5"
 echo "\
 [general]\n\
 email = \"your-email@domain.com\"\n\
